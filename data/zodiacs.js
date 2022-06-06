59ee778c5230b879020d52bf72ca538abc349efe
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-06 14:42:49 -0700

    add zodiac file in data directory will all zodiac data

---
+++ data/zodiacs.js
@@ -0,0 +1,16 @@
+const zodiacs = [
+  { id: '1', name: 'aquarius' },
+  { id: '2', name: 'aries' },
+  { id: '3', name: 'cancer' },
+  { id: '4', name: 'capricorn' },
+  { id: '5', name: 'gemini' },
+  { id: '6', name: 'leo' },
+  { id: '7', name: 'libra' },
+  { id: '8', name: 'pisces' },
+  { id: '9', name: 'sagittarius' },
+  { id: '10', name: 'scorpio' },
+  { id: '11', name: 'taurus' },
+  { id: '12', name: 'virgo' },
+];
+
+module.exports = { zodiacs };
